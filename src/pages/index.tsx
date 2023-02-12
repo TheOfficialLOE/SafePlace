87a70b9672a6c7af0dadb4aaade741fbb06843da
loe <max.shady1611@gmail.com>
2023-02-12 21:01:31 +0330

    Some linting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,11 @@
 export default function Home() {
   return <div className="max-w-[75rem] mx-auto pt-40 px-6 flex flex-col md:flex-row justify-between flex-wrap">
         <p className="text-white text-2xl leading-9 max-w-[40rem] my-6">
-            You're relaxed...
+            You&apos;re relaxed...
             <br/>
-            You're sublime...
+            You&apos;re sublime...
             <br/>
-            You're amazing... <img className="inline-block w-9 h-9" alt="halo" src="/smiling.png"/>
+            You&apos;re amazing... <img className="inline-block w-9 h-9" alt="halo" src="/smiling.png"/>
             <br/>
             Welcome to my safe<img className="inline-block w-9 h-9" alt="skull" src="/skull.png"/>place.
         </p>
@@ -19,7 +19,7 @@ export default function Home() {
                     <path fillRule="evenodd" d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z" clipRule="evenodd" />
                 </svg>
             </button>
-            <a href="https://github.com/TheOfficialLOE" target="_blank"
+            <a href="https://github.com/TheOfficialLOE" target="_blank" rel="noreferrer"
                className="mx-auto flex items-center justify-center text-white my-6 p-4 h-16 w-80
           font-bold border border-[#15151a] rounded-lg hover:border-[#353540] hover:text-[#0ce877] transition-all duration-100">
                 <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-github" width="20"
